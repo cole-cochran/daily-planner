7e3326d9d87f36e905145ac3bef12e454aa6ab5f
Cole Cochran <colecochran405@gmail.com>
2021-09-30 15:24:25 -0500

    adding logic to store user input in text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
+var calendar = $(".container");
+var saveBtn = $(".saveBtn")
+var currentTime = moment().hour();
+console.log(currentTime);
+//shows current day & time
 var today = function(){
     var day = moment();
     $("#currentDay").text(day.format("dddd, MMMM Do YYYY, hh:mm:ss a"));
     }
     setInterval(today, 1000);
-    
+
+$(".saveBtn").on("click" , saveInput);
+function saveInput() {
+    event.preventDefault;
+    var userInput = $(this).siblings("textarea").val()
+    var currentTimeText = $(this).parent().attr("clock-hour");
+    console.log("textarea", userInput, currentTimeText)
+}
+// console.log(saveInput)
+// function for time to change text area by color
+
