b6e997c31dc8246b3d91eda65a1cee11eaf02234
Cole Cochran <colecochran405@gmail.com>
2021-09-30 15:27:08 -0500

    saving user input to local storge

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,12 @@ var today = function(){
     setInterval(today, 1000);
 
 $(".saveBtn").on("click" , saveInput);
-function saveInput() {
+function saveInput(event) {
     event.preventDefault;
     var userInput = $(this).siblings("textarea").val()
     var currentTimeText = $(this).parent().attr("clock-hour");
     console.log("textarea", userInput, currentTimeText)
+    localStorage.setItem(currentTimeText, userInput)
 }
 // console.log(saveInput)
 // function for time to change text area by color
